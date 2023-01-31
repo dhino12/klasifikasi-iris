020c6bc3cf463d185b87c40050ba38e6eff2689f
dhino12 <dhinorahmad0@gmail.com>
2023-01-31 20:43:02 +0700

    test

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
-from flask import Flask, render_template, request
+import pandas as pd
 import numpy as np
 import pickle
+import streamlit as st
 
-app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+pickle_in = open('model.pkl', 'rb')
+nb = pickle.load(pickle_in)
 
-@app.route('/',methods=['GET'])
-def Home():
-    return render_template('index.html')
+def welcome():
+    return 'Selamat Datang'
 
-@app.route("/predict", methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        SepalLengthCm = float(request.form['SepalLengthCm'])
-        SepalWidthCm = float(request.form['SepalWidthCm'])
-        PetalLengthCm = float(request.form['PetalLengthCm'])
-        PetalWidthCm = float(request.form['PetalWidthCm'])
+def prediction(sepal_length, sepal_width, petal_length, petal_width):
 
+    prediction = nb.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+    print(prediction)
+    return prediction
 
-        values = np.array([[SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm]])
-        prediction = model.predict(values)
-
-
-        return render_template('index.html', prediction_text='Hasilnya adalah {}'.format(prediction))
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+def main():
+    st.title("Aplikasi Prediksi Bunga Iris Algoritma Naive Bayes")
+    st.markdown('Oleh : Agung Gunawan (2019230012) | Universitas Darma Persada | UAS Datamining')
+    st.write('\n')
+    st.markdown('Silakan isi form berikut terlebih dahulu :')
+    
+    st.write('\n')
+    sepal_length = st.text_input("Sepal Length", "0")
+    sepal_width = st.text_input("Sepal Width", "0")
+    petal_length = st.text_input("Petal Length", "0")
+    petal_width = st.text_input("Petal Width", "0")
+    result =""
+    
+    if st.button("PREDIKSI"):
+        result = prediction(sepal_length, sepal_width, petal_length, petal_width)
+    st.success('Hasil Prediksi - {}'.format(result))
+    
+if __name__=='__main__':
+    main()
